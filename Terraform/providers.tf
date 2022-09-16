0b26ccfc9ac208224cc69c6166a188c32c10e45e
Sai Kiran Karumanchi <sai.kiran.karumanchi@gds.ey.com>
2022-09-16 19:03:48 +0530

    added backend

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "3.22.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "terraformresources"
+    storage_account_name = "terraformstore275"
+    container_name       = "tfstate"
+    key                  = "prod.tfstate"
+  }
 }
 
 provider "azurerm" {
